10753894c3015eb93e29fde78cc0f5a9b539b383
Enzo <Zirotti32@gmail.com>
2022-02-13 22:04:49 -0700

    Push

--- Coin Collector/coin.py
+++ Coin Collector/coin.py
@@ -22,5 +22,43 @@ def draw():
     coin.draw()
     screen.draw.text("Score: " + str(score), color="black", topleft=(101, 10)) 
 
+    if game_over:
+        screen.fill("pink")
+        screen.draw.text("Final Score: " + str(score), topleft=(10, 10), fontsize = 60)
+# Adding function placeholders
+
+def place_coin():
+    coin.x = randint(20, (WIDTH - 20))
+    coin.y = randint(20, (HEIGHT - 20))
+def time_up():
+    global game_over
+    game_over = True
+    
+
+def update():
+
+    global score
+
+    if keyboard.left:
+        fox.x = fox.x - 2
+    elif keyboard.right:
+        fox.x = fox.x + 2
+    elif keyboard.up:
+        fox.y = fox.y - 2
+    elif keyboard.down:
+        fox.y = fox.y + 2
+    
+    coin_collected = fox.colliderect(coin)
+
+    if coin_collected:
+        
+        score = score + 10
+        place_coin()
+
+# Set the timer
+clock.schedule(time_up, 20.0)
+
+# Placing the coin
+place_coin()
 
 pgzrun.go()
