6efbd2c5d527f3366b1e54200d09e2d16d95888c
Enzo <Zirotti32@gmail.com>
2022-02-13 20:47:46 -0700

    Finish my first game!!!!

---
+++ Shoot the fruit game/shoot.py
@@ -0,0 +1,29 @@
+#Always import library's first.
+import pgzrun
+from random import  randint
+
+apple = Actor("apple")
+
+# Drawing the apple on the screen
+
+def draw():
+    screen.clear()
+    apple.draw()
+# Defining the function to place the apple
+def place_apple():
+    apple.x = randint(10, 500)
+
+    apple.y = randint(10, 500)
+
+def on_mouse_down(pos):
+    #adding some logic to the button clicks
+    if apple.collidepoint(pos):
+        print("Good Shot!")
+        place_apple()
+    else:
+        print("You missed!")
+        quit()
+
+place_apple()
+
+pgzrun.go()
