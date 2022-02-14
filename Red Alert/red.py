cbb789cfe2c1dbeb3ad27891756d496182ad59f7
Enzo <Zirotti32@gmail.com>
2022-02-14 14:53:44 -0700

    New game new update

---
+++ Red Alert/red.py
@@ -0,0 +1,27 @@
+import pgzrun
+import random
+
+
+FONT_COLOR = (255, 255, 255)
+
+WIDTH = 800
+
+HEIGHT = 600
+
+CENTER_X = WIDTH / 2
+
+CENTER_Y = HEIGHT / 2
+
+CENTER = (CENTER_X, CENTER_Y)
+
+FINAL_LEVEL = 6
+
+START_SPEED = 10
+
+COLORS = ["green", "blue"]
+
+game_over = False
+game_complete = False
+current_level = 1
+stars = []
+animations = []
