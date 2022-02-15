0d376d1b231583d3ca3a8010beb07ed90a77643d
Enzo <Zirotti32@gmail.com>
2022-02-14 23:19:50 -0700

    red.py push making stars

--- Red Alert/red.py
+++ Red Alert/red.py
@@ -25,3 +25,31 @@ game_complete = False
 current_level = 1
 stars = []
 animations = []
+## Drawing the stars
+def draw():
+    global stars, current_level, game_over, game_complete
+    screen.clear()
+    screen.blit("space", (0,0))
+    if game_over:
+        display_message("Game Over!", "Try Again")
+    elif game_complete:
+        display_message("YOU WON!!", "Well Done")
+    else:
+        for star in stars:
+            star.draw()
+## Define the update function
+
+def update():
+    global stars
+    if len(stars) == 0:
+        stars = make_stars(current_level)
+
+## Make the stars
+
+def make_stars(number_of_extra_stars):
+    colors_to_create = get_colors_to_create(number_of_extra_stars)
+    new_stars = create_stars(colors_to_create)
+    layout_stars(new_stars)
+    animate_stars(new_stars)
+    return new_stars
+
