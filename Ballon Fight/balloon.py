783ee8e4d0aa76479307fe357922e94a42a58e6a
EnzoZirotti <zirotti32@gmail.com>
2022-06-10 10:26:45 -0600

    Update

--- Ballon Fight/balloon.py
+++ Ballon Fight/balloon.py
@@ -1,3 +1,4 @@
+from pickle import TRUE
 import pgzrun
 from random import randint
 
@@ -8,3 +9,28 @@ HEIGHT = 600
 
 balloon = Actor("balloon")
 balloon.pos = 400, 300
+
+
+#Preparing obsacles
+
+
+bird = Actor("bird-up")
+bird.pos = randint(800, 1600), 460
+
+
+house = Actor("house")
+house.pos = randint(800, 1600), 460
+
+
+tree = Actor("tree")
+tree.pos = randint(800, 1600), 450
+
+
+bird_up = True
+up = False
+game_over = False
+score = 0
+number_of_updates = 0
+
+
+scores = []
